c556c6acc2e285ffe697b204dd9320425986694d
ckirkendall <ckirkendall@nehemiahsecurity.com>
2018-03-29 23:51:17 -0400

    New optimized code now generates the same attack paths.  And is up to 5 times faster than our previous optimization.

--- src/main/java/org/fiware/cybercaptor/server/scoring/math/AttackPaths.java
+++ src/main/java/org/fiware/cybercaptor/server/scoring/math/AttackPaths.java
@@ -20,6 +20,7 @@
  ****************************************************************************************/
 package org.fiware.cybercaptor.server.scoring.math;
 
+import org.fiware.cybercaptor.server.scoring.types.Arc;
 import org.fiware.cybercaptor.server.scoring.types.Graph;
 import org.fiware.cybercaptor.server.scoring.types.Vertex;
 import org.fiware.cybercaptor.server.scoring.types.VertexType;
@@ -43,11 +44,33 @@ public class AttackPaths {
     public static Graph[] main(Vertex[] Targets, Graph AttackGraph) {
 
         if (Targets != null) {
-            System.out.println(System.currentTimeMillis() + ": Preprocessing Graph");
+            //System.out.println(System.currentTimeMillis() + " (main): Preprocessing Graph");
             AttackGraph.preProcessGraph();
-            System.out.println(System.currentTimeMillis() + ": Exploring Attack Paths");
+            //System.out.println(System.currentTimeMillis() + "(main): Exploring Attack Paths");
             Graph[] GraphTable = new Graph[Targets.length];
-            Arrays.parallelSetAll(GraphTable, i -> exploreAttackPath2(Targets[i], new HashSet<>(Collections.singletonList(1))));
+            Arrays.parallelSetAll(GraphTable, i -> exploreAttackPath2(Targets[i],
+                    new HashSet<>(Collections.singletonList(Targets[i].getID()))));
+            return GraphTable;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Explore the attack path = generate the attack paths
+     *
+     * @param Targets     the targets nodes in the attack graph
+     * @param AttackGraph the attack graph
+     * @return the list of attack paths
+     */
+    public static Graph[] main2(Vertex[] Targets, Graph AttackGraph) {
+
+        if (Targets != null) {
+            //System.out.println(System.currentTimeMillis() + " (main2): Preprocessing Graph");
+            AttackGraph.preProcessGraph();
+            //System.out.println(System.currentTimeMillis() + " (main2): Exploring Attack Paths");
+            Graph[] GraphTable = new Graph[Targets.length];
+            Arrays.parallelSetAll(GraphTable, i -> exploreAttackPath(Targets[i], new HashSet<>(), AttackGraph));
             return GraphTable;
         } else {
             return null;
@@ -63,88 +86,60 @@ public class AttackPaths {
      */
     private static Graph exploreAttackPath2(Vertex V, Set<Integer> Forbidden) {
         Graph Result = null;
+        List<Graph> Buffers = new ArrayList<>();
+        List<Graph> AtomicBuffers = new ArrayList<>();
 
         // Must reset the reference so we don't modify the incoming map
         Forbidden = new HashSet<>(Forbidden);
 
         List<Vertex> predecessors = V.getPredecessors();
-        List<Graph> Buffers = new ArrayList<>();
-        List<Graph> AtomicBuffers = new ArrayList<>();
-        switch (V.getType()) {
-            case AND: {
-                if (checkForbidden(predecessors, Forbidden)) {
-                    return null;
+        if (V.getType() == VertexType.AND) {
+            if (checkForbidden(predecessors, Forbidden)) {
+                return null;
+            }
+            Result = V.getPredecessorsGraph();
+            for (Vertex D : predecessors) {
+                if (D.getType() == VertexType.LEAF) {
+                    Buffers.add(V.getPredecessorAtomicGraphs().get(D.getID()));
+                } else if (D.getType() == VertexType.OR) {
+                    Graph parentRes = exploreAttackPath2(D, Forbidden);
+
+                    //One parent of the AND is missing -> Delete the whole branch
+                    if (parentRes == null) {
+                        return null;
+                    } else {
+                        AtomicBuffers.add(V.getPredecessorAtomicGraphs().get(D.getID()));
+                        Buffers.add(parentRes);
+                    }
                 }
+            }
+        } else if (V.getType() == VertexType.OR) {
+            for (Vertex D : predecessors) {
+                if (D.getType() == VertexType.LEAF) {
+                    Buffers.add(V.getPredecessorAtomicGraphs().get(D.getID()));
+                } else if (D.getType() == VertexType.AND) {
+                    Graph parentRes = exploreAttackPath2(D, Forbidden);
+
+                    if (parentRes != null) {
+                        Graph BufferGraph = D.getSuccessorGraphs().get(V.getID());
+
+                        if (BufferGraph == null) {
+                            BufferGraph = parentRes;
+                            AtomicBuffers.add(V.getPredecessorAtomicGraphs().get(D.getID()));
+                        }
+                        Buffers.add(BufferGraph);
+                    }
+                }
+            }
+
+            if (V.getPredecessorsGraph() != null && predecessors.size() == Buffers.size()) {
                 Result = V.getPredecessorsGraph();
-                for (Vertex D : predecessors) {
-                    switch (D.getType()) {
-                        case LEAF:
-                            Buffers.add(D.getSuccessorGraphs().get(V.getID()));
-                            break;
-
-                        case OR:
-                            Graph parentRes = exploreAttackPath2(D, Forbidden);
-
-                            //One parent of the AND is missing -> Delete the whole branch
-                            if (parentRes == null) {
-                                return null;
-                            } else {
-                                AtomicBuffers.add(V.getPredecessorAtomicGraphs().get(D.getID()));
-                                Buffers.add(parentRes);
-                            }
-                            break;
-
-                        case AND:
-                            // should never happen with pre-opt
-                            break;
-                    }
-                }
             }
-            break;
-
-            case OR: {
-                for (Vertex D : predecessors) {
-                    switch (D.getType()) {
-                        case LEAF:
-                            Buffers.add(D.getSuccessorGraphs().get(V.getID()));
-                            break;
-
-                        case AND:
-                            Graph parentRes = exploreAttackPath2(D, Forbidden);
-
-                            //One parent of the AND is missing -> Delete the whole branch
-                            if (parentRes != null) {
-                                Graph BufferGraph = D.getSuccessorGraphs().get(V.getID());
-
-                                if (BufferGraph == null) {
-                                    BufferGraph = parentRes;
-                                    AtomicBuffers.add(V.getPredecessorAtomicGraphs().get(D.getID()));
-                                }
-                                Buffers.add(BufferGraph);
-                            }
-                            break;
-
-                        case OR:
-                            // Should never happen with pre-opt
-                            break;
-                    }
-                }
-
-                if (V.getPredecessorsGraph() != null && predecessors.size() == Buffers.size()) {
-                    Result = V.getPredecessorsGraph();
-                }
-            }
-            break;
-
-            case LEAF:
-                break;
         }
 
-        if (Result == null) {
-            if (!Buffers.isEmpty()) {
-                Buffers.addAll(AtomicBuffers);
-                Result = Graph.mergeGraphs(Buffers);
-            }
+        if (Result == null && !Buffers.isEmpty()) {
+            Buffers.addAll(AtomicBuffers);
+            Result = Graph.mergeGraphs(Buffers);
         }
 
         return Result;
@@ -171,4 +166,115 @@ public class AttackPaths {
         return false;
     }
 
+    private static Graph exploreAttackPath(Vertex V, Set<Integer> Forbidden, Graph graph) {
+        Graph Result = null;
+
+        // Must reset the reference so we don't modify the incoming map
+        Forbidden = new HashSet<>(Forbidden);
+
+        List<Vertex> predecessors = V.getPredecessors();
+        if (V.getType().equals(VertexType.AND)) {
+            if (predecessors != null) {
+                List<Graph> Buffers = new ArrayList<>();
+                for (Vertex D : predecessors) {
+                    if (D != null) {
+                        if (D.getType().equals(VertexType.LEAF)) {
+                            Buffers.add(createAtomicGraph(V, D));
+                        } else if (D.getType().equals(VertexType.OR)) {
+                            if (!Forbidden.contains(D.getID())) {
+                                if (Forbidden.isEmpty()) {
+                                    Forbidden.add(D.getID());
+                                } else {
+                                    Forbidden.add(D.getID());
+                                }
+                                Graph BufferGraph = createAtomicGraph(V, D);
+                                Graph parentRes = exploreAttackPath(D, Forbidden, graph);
+
+                                //One parent of the AND is missing -> Delete the whole branch
+                                if (parentRes == null) {
+                                    return null;
+                                } else {
+                                    Buffers.add(mergeGraphs(BufferGraph, parentRes));
+                                }
+                            } else {
+                                return null;
+                            }
+                        }
+                    }
+                }
+                for (Graph Buffer1 : Buffers) {
+                    if (Buffer1 == null) {
+                        return null;
+                    }
+                }
+                for (Graph Buffer : Buffers) {
+                    Result = mergeGraphs(Result, Buffer);
+                }
+            }
+            return Result;
+        }
+        if (V.getType().equals(VertexType.OR)) {
+            if ( Forbidden.isEmpty() ) {
+                Forbidden.add(V.getID());
+            }
+            if (predecessors != null) {
+                Graph Buffer = null;
+                boolean atLeastOnePath = false;
+                for (Vertex D : predecessors) {
+                    if (D != null) {
+                        if (D.getType().equals(VertexType.LEAF)) {
+                            Buffer = mergeGraphs(Buffer, createAtomicGraph(V, D));
+                            atLeastOnePath = true;
+                        } else if (D.getType().equals(VertexType.AND)) {
+                            Graph TempBuffer = exploreAttackPath(D, Forbidden, graph);
+                            if (TempBuffer != null) {
+                                Buffer = mergeGraphs(Buffer, mergeGraphs(createAtomicGraph(V, D), TempBuffer));
+                                atLeastOnePath = true;
+                            }
+                        }
+                    }
+                }
+                if (!atLeastOnePath) {
+                    return null;
+                } else
+                    return Buffer;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Merge two graphs in a new graph
+     *
+     * @param successor   the first graph
+     * @param predecessor the secon graph
+     * @return the merged graph
+     */
+    private static Graph mergeGraphs(Graph successor, Graph predecessor) {
+        if (successor == null) {
+            return predecessor;
+        }
+        if (predecessor == null) {
+            return successor;
+        }
+
+        Set<Arc>             predecessorArcs      = predecessor.getArcs();
+        Map<Integer, Vertex> predecessorVertexMap = predecessor.getVertexMap();
+
+        predecessorArcs.addAll(successor.getArcs());
+        predecessorVertexMap.putAll(successor.getVertexMap());
+
+        return new Graph(predecessorArcs, predecessorVertexMap);
+    }
+
+    /**
+     * Create an atomic graph from two vertices
+     *
+     * @param V a vertex
+     * @param D a vertex
+     * @return the new graph
+     */
+    private static Graph createAtomicGraph(Vertex V, Vertex D) {
+        return Graph.createAtomicGraph(V, D);
+    }
 }
--- src/test/java/org/fiware/cybercaptor/server/scoring/math/AttackPathsTest.java
+++ src/test/java/org/fiware/cybercaptor/server/scoring/math/AttackPathsTest.java
@@ -28,22 +28,22 @@ public class AttackPathsTest {
      */
     @Test
     public void testAttackGraphEval() throws Exception {
-        String path =
-                Paths.get(this.getClass().getClassLoader().getResource("AttackGraph.xml").toURI()).toFile().getAbsolutePath();
-
-        System.out.println(System.currentTimeMillis() + ": Loading mulval attack graph from file");
-        MulvalAttackGraph mulvalAttackGraph = new MulvalAttackGraph();
-        mulvalAttackGraph.loadFromFile(path);
-
         long startMillis = System.nanoTime();
-        int loopCount = 1;
+        int loopCount = 2;
         for (int loop = 0; loop < loopCount; loop++ ) {
+            String path =
+                    Paths.get(this.getClass().getClassLoader().getResource("AttackGraph.xml").toURI()).toFile().getAbsolutePath();
 
+            //System.out.println(System.currentTimeMillis() + ": Loading mulval attack graph from file");
+            MulvalAttackGraph mulvalAttackGraph = new MulvalAttackGraph();
+            mulvalAttackGraph.loadFromFile(path);
+
+            System.out.println(System.currentTimeMillis() + " (NEW): Start attack path generation");
             Arc[] ArcsTable = new Arc[mulvalAttackGraph.arcs.size()];
             Vertex[] VerticesTable = new Vertex[mulvalAttackGraph.getNumberOfVertices()];
 
             int i = 0;
-            System.out.println(System.currentTimeMillis() + ": Setup attack path graph");
+            //System.out.println(System.currentTimeMillis() + ": Setup attack path graph");
             for (Integer key : mulvalAttackGraph.vertices.keySet()) {
                 org.fiware.cybercaptor.server.attackgraph.Vertex vertex = mulvalAttackGraph.vertices.get(key);
                 VerticesTable[i] = new Vertex(-1, "EOF", -1, VertexType.LEAF);
@@ -69,16 +69,93 @@ public class AttackPathsTest {
             Graph graph = new Graph(ArcsTable, VerticesTable);
             Vertex[] TargetSet = Graph.getVerticesOnTypeAndFact(VerticesTable, VertexType.OR);
 
-            System.out.println(System.currentTimeMillis() + ": Generate Attack Paths");
+            //System.out.println(System.currentTimeMillis() + ": Generate Attack Paths");
             Graph[] result = AttackPaths.main(TargetSet, graph); //Disabled following the test launch of attack path algorithm.
 
-            System.out.println(System.currentTimeMillis() + ": Scoring Attack Paths");
+            //System.out.println(System.currentTimeMillis() + ": Scoring Attack Paths");
             ScoringFormulas formulas = new ScoringFormulas();
-            double scoreAttackGraph = formulas.MinMax(formulas.globalScore(graph), 0);
+            double scoreAttackGraph = formulas.MinMax(formulas.globalScore(graph), 1);
+            //System.out.println(System.currentTimeMillis() + ": Attack path score: " + scoreAttackGraph);
+
+            int count = 0;
+            for (Graph attackPath : result) {
+                if ( attackPath.getVertexMap().size() > 3) {
+                    count++;
+                }
+                //System.out.println(System.currentTimeMillis() + ": AttackPath(arcs:" + attackPath.getArcs().size() +
+                //        ", vertices:" + attackPath.getVertexMap().size() + ")");
+            }
+            System.out.println(System.currentTimeMillis() + " (NEW): Number of attack paths - " + count);
+        }
+        System.out.println("(NEW) Total time with " + loopCount + " loops: " + (System.nanoTime() - startMillis));
+    }
+
+    /**
+     * Test attack graph evaluation
+     */
+    @Test
+    public void testAttackGraphEval2() throws Exception {
+        long startMillis = System.nanoTime();
+        int loopCount = 2;
+        for (int loop = 0; loop < loopCount; loop++ ) {
+            String path =
+                    Paths.get(this.getClass().getClassLoader().getResource("AttackGraph.xml").toURI()).toFile().getAbsolutePath();
+
+            //System.out.println(System.currentTimeMillis() + ": Loading mulval attack graph from file");
+            MulvalAttackGraph mulvalAttackGraph = new MulvalAttackGraph();
+            mulvalAttackGraph.loadFromFile(path);
+
+            System.out.println(System.currentTimeMillis() + ": Start attack path generation");
+            Arc[] ArcsTable = new Arc[mulvalAttackGraph.arcs.size()];
+            Vertex[] VerticesTable = new Vertex[mulvalAttackGraph.getNumberOfVertices()];
+
+            int i = 0;
+            //System.out.println(System.currentTimeMillis() + ": Setup attack path graph");
+            for (Integer key : mulvalAttackGraph.vertices.keySet()) {
+                org.fiware.cybercaptor.server.attackgraph.Vertex vertex = mulvalAttackGraph.vertices.get(key);
+                VerticesTable[i] = new Vertex(-1, "EOF", -1, VertexType.LEAF);
+                VerticesTable[i].setID(vertex.id);
+                VerticesTable[i].setFact(vertex.fact.factString);
+                VerticesTable[i].setMulvalMetric(vertex.mulvalMetric);
+                VerticesTable[i].setType(VertexType.valueOf(vertex.type.toString().toUpperCase()));
+                ImpactMetric[] impactMetrics = new ImpactMetric[vertex.impactMetrics.size()];
+                for (int j = 0; j < vertex.impactMetrics.size(); j++) {
+                    impactMetrics[j] = vertex.impactMetrics.get(j);
+                }
+                VerticesTable[i].setImpactMetrics(impactMetrics);
+                i++;
+            }
+
+            for (int j = 0; j < mulvalAttackGraph.arcs.size(); j++) {
+                org.fiware.cybercaptor.server.attackgraph.Arc arc = mulvalAttackGraph.arcs.get(j);
+                ArcsTable[j] = new Arc(-1, -1);
+                ArcsTable[j].setSource(arc.destination.id);
+                ArcsTable[j].setDestination(arc.source.id);
+            }
+
+            Graph graph = new Graph(ArcsTable, VerticesTable);
+            Vertex[] TargetSet = Graph.getVerticesOnTypeAndFact(VerticesTable, VertexType.OR);
+
+            //System.out.println(System.currentTimeMillis() + ": Generate Attack Paths");
+            Graph[] result = AttackPaths.main2(TargetSet, graph); //Disabled following the test launch of attack path algorithm.
+
+            //System.out.println(System.currentTimeMillis() + ": Scoring Attack Paths");
+            ScoringFormulas formulas = new ScoringFormulas();
+            double scoreAttackGraph = formulas.MinMax(formulas.globalScore(graph), 1);
+            //System.out.println(System.currentTimeMillis() + ": Attack path score: " + scoreAttackGraph);
+
+            int count = 0;
+            for (Graph attackPath : result) {
+                if ( attackPath.getVertexMap().size() > 3) {
+                    count++;
+                }
+                //System.out.println(System.currentTimeMillis() + ": AttackPath(arcs:" + attackPath.getArcs().size() +
+                //", vertices:" + attackPath.getVertexMap().size() + ")");
+            }
+            System.out.println(System.currentTimeMillis() + ": Number of attack paths - " + count);
         }
         System.out.println("Total time with " + loopCount + " loops: " + (System.nanoTime() - startMillis));
     }
-
     /**
      * Test merging.
      */
